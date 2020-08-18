038ac31dac7f585cc0d2971ccea464b0c00c8d76
leonardo <leonardo@pop-os.localdomain>
2020-08-18 20:21:09 -0300

    feat: ensure client creation is working

--- clients/ruby/main_test.rb
+++
@@ -1,7 +0,0 @@
-class Nothing
-  context 'description' do
-    it 'does something' do
-      expect(2 + 2).to equal(4)
-    end
-  end
-end
---
+++ clients/ruby/src/client.rb
@@ -0,0 +1,8 @@
+class Client
+  attr_accessor :name, :purchase
+
+  def initialize name, purchase
+    @name = name
+    @purchase = purchase
+  end
+end
